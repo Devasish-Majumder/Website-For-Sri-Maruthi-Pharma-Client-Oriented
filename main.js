f52fba53968311c5e604eaac226f1e42fdef2d01
Devasish Majumder <74971386+Devasish-Majumder@users.noreply.github.com>
2022-09-25 11:28:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const toggle = document.querySelector(".toggle");
+const close = document.querySelector(".close");
+const menu = document.querySelector(".nav-items");
+
+toggle.addEventListener("click", () => {
+  menu.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  menu.classList.remove("show");
+});
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(`.home_content, .product, .patners, .items, .testimonial, .section`, {
+  interval: 200,
+});
